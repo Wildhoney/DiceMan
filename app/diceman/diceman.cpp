dcd065812feb0d9d6369e701340786649731ae75
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-13 23:05:45 +0000

    Reverted folder hierarchy

---
+++ app/diceman/diceman.cpp
@@ -0,0 +1,10 @@
+#include "diceman.hpp"
+
+/**
+ * @method getTasks
+ * @param double limit
+ * @return double
+ */
+double DiceMan::getTasks(double limit) {
+    return limit;
+}
---
+++ app/diceman/diceman.hpp
@@ -0,0 +1,20 @@
+/**
+ * @class DiceMan
+ */
+class DiceMan {
+
+    /**
+     * @property tasks
+     */
+    char tasks[4][100];
+
+    public:
+
+    /**
+     * @method getTasks
+     * @param double limit
+     * @return double
+     */
+    double getTasks(double limit);
+
+};
