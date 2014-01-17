15b3df68fcf2c85cd80355aa49d633639f761447
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-17 21:31:30 +0000

    Added initial implementation of choosing tasks

--- default.rb
+++ default.rb
@@ -13,7 +13,7 @@ end
 # Create an instance of the task object.
 dice_man = DiceMan::Tasks.new
 
-if opts.has_key?(:add)
+if opts.has_key?(:add) && opts[:add]
 
   # Gather the username to be associated with the task.
   print "What alias should we go for? "
@@ -26,6 +26,30 @@ if opts.has_key?(:add)
   # We'll begin the process of adding a new task.
   dice_man.add_task(username, task)
 
-elsif opts.has_key?(:begin)
+elsif opts.has_key?(:start) && opts[:start]
+
+  print "Please choose a task to add:\n"
+  print "If you'd like more than one, specify as: 1,3,4.\n\n"
+
+  # Initialise the variables.
+  chosen_tasks = []
+  possible_tasks = []
+  split_on = /,/
+
+  # Obtain the tasks from Firebase, and allow the user to choose some.
+  dice_man.get_tasks(5).each_with_index do |task, index|
+    selection_number = (index + 1)
+    possible_tasks[selection_number] = task
+    print " #{selection_number}. #{task['task']}\n"
+  end
+
+  # Iterate over each selection the user has made, and attempt to map that to a task
+  # from the Firebase collection.
+  gets.strip.split(split_on).each do |value|
+    task = possible_tasks[value.to_i] unless possible_tasks[value.to_i].nil?
+    chosen_tasks.push task['task'] unless task.nil?
+  end
+
+  print "You've chosen #{chosen_tasks.length} tasks..."
 
 end
--- module/Tasks.rb
+++ module/Tasks.rb
@@ -1,15 +1,27 @@
 module DiceMan
   class Tasks < Base
 
+    # @constant PATH_TASKS
+    # @type string
+    PATH_TASKS = "tasks"
+
     # @method addTask
     # @param username
     # @param task
     # @return boolean
     def add_task(username, task)
+      @firebase.push(PATH_TASKS, { :username => username, :task => task })
+    end
 
-      response = @firebase.push("tasks", { :username => username, :task => task })
-      print response
-
+    # @method getTasks
+    # @param limit
+    # @return array
+    def get_tasks(limit)
+      tasks = []
+      @firebase.get(PATH_TASKS).body.each_pair do |key, task|
+        tasks.push task
+      end
+      tasks
     end
 
   end
