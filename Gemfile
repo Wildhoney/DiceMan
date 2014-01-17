1c84f17e1e9359d136d4a20dba0280116bab8106
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-17 00:12:53 +0000

    Added Trollop dependency for opt param parsing

--- Gemfile
+++ Gemfile
@@ -1,1 +1,2 @@
 gem 'firebase', '0.1.6'
+gem 'trollop', '2.0'
--- default.rb
+++ default.rb
@@ -1,5 +1,11 @@
+require 'trollop'
 require './module/main.rb'
 require './module/output.rb'
+
+opts = Trollop::options do
+  opt :add, "Add your own task for others to fulfill", :short => 'a'
+  opt :begin, "Compile a list of possible tasks and roll the dice", :short => 'b'
+end
 
 diceMan = DiceMan::Main.new
 print diceMan
